1dfb8ff8fc00114324bca514956c3cec933b0b0d
Beatrix <beatrix.bauer@gmail.com>
2025-02-21 10:56:12 +0100

    Initial Commit

---
+++ Radio_Sphere/Radio_SphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  Radio_SphereApp.swift
+//  Radio_Sphere
+//
+//  Created by Beatrix Bauer on 21.02.25.
+//
+
+import SwiftUI
+
+@main
+struct Radio_SphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Radio_SphereTests/Radio_SphereTests.swift
@@ -0,0 +1,17 @@
+//
+//  Radio_SphereTests.swift
+//  Radio_SphereTests
+//
+//  Created by Beatrix Bauer on 21.02.25.
+//
+
+import Testing
+@testable import Radio_Sphere
+
+struct Radio_SphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
