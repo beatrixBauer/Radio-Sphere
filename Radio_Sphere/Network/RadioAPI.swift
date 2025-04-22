d0396609e5a4902aeb4fb57afb325f4b11dc283b
Beatrix <beatrix.bauer@gmail.com>
2025-04-22 08:59:44 +0200

    Änderung an der API-Abfrage

--- Radio_Sphere/Network/RadioAPI.swift
+++ Radio_Sphere/Network/RadioAPI.swift
@@ -18,7 +18,7 @@ class RadioAPI {
         return availableBaseURLs.first! + "/json/stations"
     }
     
-    private let userAgent = "Radio Sphere/0.9 (iOS; beatrix.bauer@gmail.com)"
+    private let userAgent = "Radio Sphere/1.0 (iOS; beatrix.bauer@gmail.com)"
 
     // Initializer mit übergebener URLSession – Standard ist URLSession.shared
     init(session: URLSession = .shared) {
